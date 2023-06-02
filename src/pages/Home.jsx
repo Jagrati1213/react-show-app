e2b0705a1e4167f605cd5b4f1d993ffbc3509b24
Jagrati Gupta <123jagratigupta@gmail.com>
2023-06-02 15:19:25 +0530

    jg: call card show

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardShow from '../components/showCard/CardShow';
+
+function Home() {
+  return (
+    <main className="home mt-[-90px] container mx-auto">
+      <CardShow title={'Jewelery'} />
+    </main>
+  );
+}
+
+export default Home;
