003c326f93c978d0cfd48a9707694d505b751032
Jagrati Gupta <123jagratigupta@gmail.com>
2023-06-03 00:33:47 +0530

    jg: remove title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import CardShow from '../components/showCard/CardShow';
 function Home() {
   return (
     <main className="home mt-[-90px] container mx-auto">
-      <CardShow title={'Jewelery'} />
+      <CardShow />
     </main>
   );
 }
